1a5c154bace1aa66e0449823b7fd5ed49d6d261c
Bishal <bishals10@gmail.com>
2024-02-27 01:14:50 +0200

    --amend

--- src/App.jsx
+++ src/App.jsx
@@ -36,11 +36,6 @@ function App() {
 
   function handleStopRemovePlace() {
     modal.current.close();
-    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
-    localStorage.setItem(
-      "selectedPlace",
-      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
-    );
   }
 
   function handleSelectPlace(id) {
@@ -51,6 +46,7 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
     const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
     if (storedIds.indexOf(id) === -1) {
       localStorage.setItem("selectedPlaces", JSON.stringify([id, ...storedIds]));
@@ -62,6 +58,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem(
+      "selectedPlace",
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
